2cfd31326a22466641ba88e6a93169c10d928191
mohammad <mahbub.rahman0204@gmail.com>
2017-12-16 22:33:23 +0000

    sfs

---
+++ bluetooth2.py
@@ -0,0 +1,65 @@
+import os
+import glob
+import time
+import RPi.GPIO as GPIO
+from bluetooth import *
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+
+
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "AquaPiServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
+                    )
+client_sock, client_info = server_sock.accept()
+while True:          
+	print "Waiting for connection on RFCOMM channel %d" % port
+
+	
+	print "Accepted connection from ", client_info
+
+	try:
+	        data = client_sock.recv(1024)
+        	if len(data) == 0: break
+	        print "received [%s]" % data
+
+		
+		if data == 'lightOn':
+			GPIO.output(17,False)
+			data = 'light on!'
+		elif data == 'lightOff':
+			GPIO.output(17,True)
+			data = 'light off!'
+		else:
+			data = 'WTF!' 
+			client_sock.send(data)
+		print "sending [%s]" % data
+
+	except IOError:
+		pass
+
+	except KeyboardInterrupt:
+
+		print "disconnected"
+
+		client_sock.close()
+		server_sock.close()
+		print "all done"
+
+		break
--- readme.txt
+++ readme.txt
@@ -23,3 +23,18 @@ Install Samba
 		directory mask = 0777
 		Public = yes
 		Guest ok = yes
+
+To install vim
+	sudo apt-get install vim
+
+bluetooth work
+	sudo apt-get install bluez python-bluez
+	sudo vim /etc/bluetooth/main.conf
+	add this line
+		DisablePlugins = pnat
+	To make your pi bluetooth Discoverable
+		sudo hciconfig hci0 piscan
+	[change your device name to something else you fancy]
+		sudo hciconfig hci0 name 'ourbot' 
+
+https://www.youtube.com/watch?v=DmtJBc229Rg
